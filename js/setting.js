47493ad3cfc19987324393fb470142f97a165826
Issei32 <hoangtrong0302@gmail.com>
2024-11-03 19:26:30 +0700

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,19 +1,18 @@
-// Get the modal elements
 const modal = document.getElementById("settings-modal");
 const settingsButton = document.getElementById("settings-btn");
 const closeButton = document.querySelector(".close-button");
 
-// Open the modal when the settings button is clicked
+// Nhấn nút cài đặt sẽ mở modal
 settingsButton.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
-// Close the modal when the close button is clicked
+// Nhấn nút tắt để tắt modal
 closeButton.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// Close the modal when clicking outside the modal content
+// Tắt modal khi nhấn ra ngoài modal
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
     modal.style.display = "none";
