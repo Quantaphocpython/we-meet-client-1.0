34d3fa6a36b35b1033ec9a3e2f4e928eae937be3
Tran Hoang Quan <quan02042004@gmail.com>
2024-11-10 01:08:10 +0700

    :rocket: deploy

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const base_url = 'http://localhost:8080/we-meet';
+export const base_url = 'https://we-meet-server-1-0-0.onrender.com/we-meet';
 
 export function getAccessToken() {
   const accessToken = localStorage.getItem('accessToken') ?? '';
